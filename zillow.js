f39921a73d3092439058506793100b13ad818767
AllanDroter <allan.droter@gmail.com>
2024-04-06 21:56:46 -0400

    attempting to give a proper error message for empty zestimate field and failing

--- zillow.js
+++ zillow.js
@@ -7,21 +7,26 @@ app.get('/', function(req, res){
     res.status(200).send("root");
 });
 
+//cheeky flavor text
+app.get('/v1/zillow', function(req, res){
+    res.status(200).send("Welcome to Zillow");
+});
+
 // Write a server called zillow.js that serves JSON data when it receives a GET request to the path ’/v1/zillow/zestimate’. 
 app.get('/v1/zillow/zestimate', (req, res) => {
-    if(req.query != null){
-        // Expect the request to contain a query string with a keys ’sqft’, ’bed’, and ’bath’ all of which will be required integers.
-        var {sqft, bed, bath} = req.query;
+    // Expect the request to contain a query string with a keys ’sqft’, ’bed’, and ’bath’ all of which will be required integers.
+    const {sqft, bed, bath} = req.query;
 
-        //Zestimate == sqft * bed * bath * 10 and you should return to the use JSON in the following format: {zestimate: Number }
-        var zestimate = sqft * bed * bath * 10;
+    //Zestimate == sqft * bed * bath * 10 and you should return to the use JSON in the following format: {zestimate: Number }
+    var zestimate = sqft * bed * bath * 10;
 
+    if(zestimate.parameter == null){
+        // why does this refuse to work
+        res.status(404).send("Invalid argument").json([]);
+    }else{
         // The JSON response should contain only the ‘zestimate’ property:
         res.status(200).json({zestimate});
-    }else{
-        res.status(404).send("Invalid argument");
-    }
-});
+    }});
 // For example: /v1/zillow/zestimate?sqft=2000&bed=3&bath=4
 // For example: {“zestimate”: 240000}
 
@@ -36,14 +41,12 @@ app.get('/v1/zillow/houses', (req, res) => {
     }else{
         const resultsArr = sampleDB.filter(sampleDB => sampleDB.city === city);
 
-        if(resultsArr != null){
-            res.status(200).json(resultsArr);
-        }else{
-            // If a city is provided and the city is not found, return an empty array [].
-            // this actual code just doesn't do anything, but the code itself functions as asked technically
+        if(resultsArr.length === 0){
             res.status(200).json([]);
         }
-    }});
+        if(resultsArr != null){
+            res.status(200).json(resultsArr);
+        }}});
 // For example: /v1/zillow/houses?city=baltimore
 // The JSON response should contain only the the list of houses in baltimore.
 // For example: [{ price: 240000, city: ”baltimore” }, { price: 325000, city: ”baltimore” }]
@@ -57,20 +60,21 @@ app.get('/v1/zillow/houses', (req, res) => {
 // Add a third endpoint at the path ‘/v1/zillow/prices’ that accepts a requiredparameter ‘usd‘.
 // This will return all houses equal to or under a given price. 
 app.get('/v1/zillow/prices', (req, res) => {
-    if(req.query.usd ){
-        const usd = req.query.usd;
-        const resultsArr = sampleDB.filter(sampleDB => sampleDB.price <= usd);
+    const usd = req.query.usd;
 
-        if(resultsArr != null){
-            res.status(200).json(resultsArr);
-        }else{
-            // If no houses are under the given price, return an empty array: []
-            res.status(200).send("No houses under given price").json([]);
-        }
-    }else{
+    if(!usd){
         res.status(404).send("Invalid argument for price point");
     }
-});
+    if(usd){
+        const resultsArr = sampleDB.filter(sampleDB => sampleDB.price <= usd);
+
+        if(resultsArr.length === 0){
+            // If no houses are under the given price, return an empty array: []
+            res.status(200).json([]);
+        }
+        if(resultsArr != null){
+            res.status(200).json(resultsArr);
+        }}});
 
 // For example: /v1/zillow/prices?usd=300000
 // The JSON response should contain: [{ price: 240000, city: ”baltimore” }, {
